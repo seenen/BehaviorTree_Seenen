b608b870ed9478e1beae01a3bc80ee370c19ecb6
seenen <44057536@qq.com>
2016-02-03 13:25:17 +0800

    添加工具
    添加批量 BehaviorTree 与 ExternalBehaviorTree的.asset之间的互相转换工具

--- Behavior-Designer/Assets/BDRuntimeExtend/Editor/Windows/BPWindow.cs
+++ Behavior-Designer/Assets/BDRuntimeExtend/Editor/Windows/BPWindow.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.IO;
 using BehaviorDesigner.Editor;
 using BehaviorDesigner.Runtime;
+using System.Collections;
+using System.IO;
+using UnityEditor;
 
 namespace BDRuntimeExtend
 {
@@ -33,18 +36,78 @@ namespace BDRuntimeExtend
 			
 			if (GUILayout.Button("Build", GUILayout.ExpandWidth(true)))
             {
-                BuildCommon();
+                Build();
             }
 
+			
+			if (GUILayout.Button("Restore", GUILayout.ExpandWidth(true)))
+            {
+                Restore();
+            }
         }
 
-        void BuildCommon()
+        //  怪物的AI存放地址.
+        public static string ASSETBUNDLE_PATH = "Assets/Resources/Monster/";
+
+        //  把 BehaviorTree 存放成 ExternalBehaviorTree 的.asset文件
+        void Build()
         {
             foreach (GameObject go in Selection.gameObjects)
             {
                 Debug.Log(go.name);
 
                 BehaviorTree bt = go.GetComponent<BehaviorTree>();
+
+                ExternalBehaviorTree holder = ScriptableObject.CreateInstance<ExternalBehaviorTree>();
+                holder.BehaviorSource = bt.GetBehaviorSource();
+
+                AssetDatabase.CreateAsset(holder, ASSETBUNDLE_PATH + bt.BehaviorName + ".asset");
+
+                AssetDatabase.Refresh();
+            }
+
+        }
+
+        //  把 ExternalBehaviorTree 还原成 BehaviorTree
+        void Restore()
+        {
+            string assetPath = "";
+            string RESOURCESPATH = "Assets/Resources/";
+
+            foreach (Object go in Selection.objects)
+            {
+                Debug.Log(go.name);
+
+                assetPath = Path.GetFullPath(AssetDatabase.GetAssetPath(go));
+
+                assetPath = assetPath.Replace("\\", "/");
+
+                Debug.Log("assetPath = " + assetPath);
+
+                //ExternalBehaviorTree o = (ExternalBehaviorTree)AssetDatabase.LoadAssetAtPath(assetPath, typeof(ExternalBehaviorTree));
+                //Debug.Log("o = " + o);
+
+                ExternalBehaviorTree ebt = null;
+
+                if (assetPath.Contains(RESOURCESPATH))
+                {
+                    int index = assetPath.LastIndexOf(RESOURCESPATH);
+                    Debug.Log("index = " + index);
+                    assetPath = assetPath.Substring(index + RESOURCESPATH.Length);
+                    Debug.Log("assetPath = " + assetPath);
+                    string endfix = Path.GetExtension(assetPath);
+                    assetPath = assetPath.Substring(0, assetPath.Length - endfix.Length);
+                    Debug.Log("assetPath = " + assetPath);
+
+                    ebt = Resources.Load(assetPath, typeof(ExternalBehaviorTree)) as ExternalBehaviorTree;
+                    Debug.Log("ebt = " + ebt);
+                }
+
+                GameObject obj = new GameObject(ebt.GetBehaviorSource().behaviorName + "_Template");
+
+                BehaviorTree bt = obj.AddComponent<BehaviorTree>();
+                bt.SetBehaviorSource(ebt.GetBehaviorSource());
+                bt.GetBehaviorSource().TaskData = ebt.GetBehaviorSource().TaskData;
 
             }
         }
